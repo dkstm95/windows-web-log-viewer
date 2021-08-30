5c232aebbcd1c304eac7570eeae5b2bcf5aead78
seungilahn <dkstm95@naver.com>
2021-08-30 13:15:15 +0900

    First Commit.

---
+++ src/main/java/com/cj/WindowsWebLogViewer/WindowsWebLogViewerApplication.java
@@ -0,0 +1,13 @@
+package com.cj.WindowsWebLogViewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindowsWebLogViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindowsWebLogViewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cj/WindowsWebLogViewer/WindowsWebLogViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cj.WindowsWebLogViewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindowsWebLogViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
